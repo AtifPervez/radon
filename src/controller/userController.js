715271136bf16220f02f3a78956bdb2057275823
AtifPervez <atifpervez0@gmail.com>
2022-07-06 12:48:15 +0530

    myProject3-userModel

---
+++ src/controller/userController.js
@@ -0,0 +1,20 @@
+// const userModel=require("../models/userModel")
+
+
+// const createUser=async function(req,res){
+//     try
+//     {
+//         let data=req.body;
+//         if(!object.keys(data).length==0){
+//             return res.status(400)
+//         }
+
+
+//     }
+//     catch
+//     {
+
+//     }
+
+// }
+// module.exports={createUser};
